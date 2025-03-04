dfcba23aa1d95cdf488c8fecd2f0f2ea7c0e440b
DrixNL08 <97118963@st.deltion.nl>
2025-03-04 15:00:51 +0100

    niks  veranderd

--- mysterie van het doolhof/Assets/Beau/Back.cs
+++ mysterie van het doolhof/Assets/Beau/Back.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Back: MonoBehaviour
+public class Back : MonoBehaviour
 {
 
     public void PlayGame()
