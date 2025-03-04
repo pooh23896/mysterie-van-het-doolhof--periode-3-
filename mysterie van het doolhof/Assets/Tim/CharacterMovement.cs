83c47db2550b7010c88528a3c2df0a3d541e302c
TimLiefers505 <97114065@st.deltion.nl>
2025-03-04 15:10:39 +0100

    script aangepast

--- mysterie van het doolhof/Assets/Tim/CharacterMovement.cs
+++ mysterie van het doolhof/Assets/Tim/CharacterMovement.cs
@@ -10,30 +10,29 @@ public class Movement : MonoBehaviour
     public float rotateSpeed;
     public Transform cam;
     public Vector3 camRotate;
+    public static bool canMove = true; // Controleert of de speler kan bewegen
+
     // Start is called before the first frame update
     void Start()
     {
-
     }
 
     // Update is called once per frame
     void Update()
     {
-        moveDir.x = Input.GetAxis("Horizontal");
-        moveDir.z = Input.GetAxis("Vertical");
+        if (canMove)
+        {
+            moveDir.x = Input.GetAxis("Horizontal");
+            moveDir.z = Input.GetAxis("Vertical");
 
-        transform.Translate(moveDir * Time.deltaTime * moveSpeed);
-
-
-        bodyRotate.y = Input.GetAxis("Mouse X");
-        transform.Rotate(bodyRotate * Time.deltaTime * rotateSpeed);
-
-        camRotate.x = Input.GetAxis("Mouse Y");
-        cam.Rotate(-camRotate * Time.deltaTime * rotateSpeed);
+            transform.Translate(moveDir * Time.deltaTime * moveSpeed);
 
 
+            bodyRotate.y = Input.GetAxis("Mouse X");
+            transform.Rotate(bodyRotate * Time.deltaTime * rotateSpeed);
 
-
-
+            camRotate.x = Input.GetAxis("Mouse Y");
+            cam.Rotate(-camRotate * Time.deltaTime * rotateSpeed);
+        }
     }
 }
