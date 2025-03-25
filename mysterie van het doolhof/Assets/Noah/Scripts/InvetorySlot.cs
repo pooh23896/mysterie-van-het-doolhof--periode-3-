333e3c7554e654cf476216174ae9be992d3b02bd
pooh23896 <97116980@st.deltion.nl>
2025-03-25 12:41:56 +0100

    idk

--- mysterie van het doolhof/Assets/Noah/Scripts/InvetorySlot.cs
+++ mysterie van het doolhof/Assets/Noah/Scripts/InvetorySlot.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
 public class InvetorySlot : MonoBehaviour, IDropHandler
-{   public void OnDrop(PointerEventData eventData)
+{
+    public void OnDrop(PointerEventData eventData)
     {
         if (transform.childCount == 0)
         {
