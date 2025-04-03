7e9978e1fe8c2d1fc11405de0c2e4c417a21ae4a
TimLiefers505 <97114065@st.deltion.nl>
2025-04-03 15:39:35 +0200

    save and load werkt

--- mysterie van het doolhof/Assets/Beau/savingscript.cs
+++ mysterie van het doolhof/Assets/Beau/savingscript.cs
@@ -2,44 +2,67 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class Save : MonoBehaviour
 {
     public GameObject player;
-    
+    private CharacterController controller;
 
-    public void save()
-
+    void Start()
     {
-        var xPos = player.transform.position.x;   // coördinaten worden opgehaald position 
+        controller = player.GetComponent<CharacterController>();  // Haal CharacterController op
+    }
+
+    public void SavePosition()
+    {
+        var xPos = player.transform.position.x;
         var yPos = player.transform.position.y;
         var zPos = player.transform.position.z;
-        PlayerPrefs.SetFloat("X", xPos);        // waarden in opgeslagen
+        PlayerPrefs.SetFloat("X", xPos);
         PlayerPrefs.SetFloat("Y", yPos);
         PlayerPrefs.SetFloat("Z", zPos);
+        PlayerPrefs.Save();  // Opslaan forceren
+        print("Spelerpositie opgeslagen!");
     }
 
-
-
-        public void LoadPosition()
+    public void LoadPosition()
     {
-      player.transform.position = new Vector3(PlayerPrefs.GetFloat("X"),PlayerPrefs.GetFloat("Y"),PlayerPrefs.GetFloat("Z"));       // worden de opgeslagen waarden opgehaald
-
+        if (PlayerPrefs.HasKey("X") && PlayerPrefs.HasKey("Y") && PlayerPrefs.HasKey("Z"))
+        {
+            StartCoroutine(LoadWithController());
+        }
+        else
+        {
+            print("Geen opgeslagen positie gevonden!");
+        }
     }
+
+    IEnumerator LoadWithController()
+    {
+        controller.enabled = false; // Schakel CharacterController uit om directe positie-aanpassing toe te staan
+        yield return new WaitForEndOfFrame(); // Wacht een frame om errors te voorkomen
+
+        player.transform.position = new Vector3(
+            PlayerPrefs.GetFloat("X"),
+            PlayerPrefs.GetFloat("Y"),
+            PlayerPrefs.GetFloat("Z")
+        );
+
+        yield return new WaitForEndOfFrame(); // Wacht opnieuw een frame
+
+        controller.enabled = true; // Schakel CharacterController weer in
+        print("Spelerpositie geladen!");
+    }
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.G)) // Druk op "G" om op te slaan
         {
-            save();
-            print ("Spelerpositie opgeslagen!");
+            SavePosition();
         }
 
         if (Input.GetKeyDown(KeyCode.L)) // Druk op "L" om te laden
         {
             LoadPosition();
-            print("Spelerpositie geladen!");  
         }
     }
-
 }
-
