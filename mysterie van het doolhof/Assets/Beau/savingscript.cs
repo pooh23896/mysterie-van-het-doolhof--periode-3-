35e6f0fdad5afbe0d50ee033d618f1b1ba462e61
DrixNL08 <97118963@st.deltion.nl>
2025-04-03 14:20:25 +0200

    material

--- mysterie van het doolhof/Assets/Beau/savingscript.cs
+++ mysterie van het doolhof/Assets/Beau/savingscript.cs
@@ -11,10 +11,10 @@ public class Save : MonoBehaviour
     public void save()
 
     {
-        var xPos = player.transform.position.x;
+        var xPos = player.transform.position.x;   // coördinaten worden opgehaald position 
         var yPos = player.transform.position.y;
         var zPos = player.transform.position.z;
-        PlayerPrefs.SetFloat("X", xPos);
+        PlayerPrefs.SetFloat("X", xPos);        // waarden in opgeslagen
         PlayerPrefs.SetFloat("Y", yPos);
         PlayerPrefs.SetFloat("Z", zPos);
     }
@@ -23,10 +23,23 @@ public class Save : MonoBehaviour
 
         public void LoadPosition()
     {
-        player.transform.position = new Vector3(PlayerPrefs.GetFloat("X"),PlayerPrefs.GetFloat("Y"),PlayerPrefs.GetFloat("Z"));       
+      player.transform.position = new Vector3(PlayerPrefs.GetFloat("X"),PlayerPrefs.GetFloat("Y"),PlayerPrefs.GetFloat("Z"));       // worden de opgeslagen waarden opgehaald
 
     }
- 
-    
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.G)) // Druk op "S" om op te slaan
+        {
+            save();
+            Debug.Log("Spelerpositie opgeslagen!");
+        }
+
+        if (Input.GetKeyDown(KeyCode.L)) // Druk op "L" om te laden
+        {
+            LoadPosition();
+            Debug.Log("Spelerpositie geladen!");  
+        }
+    }
+
 }
 
